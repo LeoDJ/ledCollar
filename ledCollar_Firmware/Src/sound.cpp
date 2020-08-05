623e1fea8ce71927bcb8ee73f7f753c3f71ed7e8
LeoDJ <leandro.spaeth@gmail.com>
2020-08-06 00:30:39 +0200

    add trough filter

--- ledCollar_Firmware/Src/sound.cpp
+++ ledCollar_Firmware/Src/sound.cpp
@@ -11,7 +11,7 @@ uint16_t micSamples[NUM_MIC_SAMPLES];
 uint16_t micMaxVal = 0;
 
 GoertzelFilter filter = GoertzelFilter(TARGET_FREQ, BLOCK_SIZE, SAMPLING_RATE);
-float midpointAvg = 0, peakVal = 0;
+float midpointAvg = 0, peakVal = 0, troughVal = 0;
 uint32_t lastPeak;
 uint16_t bassSmoothValues[BASS_SMOOTH_COUNT];
 uint8_t bassSmoothIndex = 0;
@@ -42,7 +42,7 @@ void adcTransferComplete() {
     // flip the negative part of the waveform to the top and make it begin at 0
     float absVal = filterVal > midpointAvg ? filterVal - midpointAvg : midpointAvg - filterVal;
 
-    // smooth between the values so the LED strip has more stable data
+    // smooth between the values so the LED strip has more stable data, basically storing a local maximum
     bassSmoothValues[bassSmoothIndex] = absVal;
     bassSmoothIndex++;
     if(bassSmoothIndex >= BASS_SMOOTH_COUNT) {
@@ -68,8 +68,18 @@ void adcTransferComplete() {
         peakVal = BASS_PEAK_MIN_VAL;
     }
 
-    // scale the output to a single byte without overshoot
-    float scaledOutput = (MIN(smoothVal, peakVal) / peakVal) * 255.0;
+    // keep a minimum trough value to react better to a constant bassline
+    if(smoothVal < troughVal) {
+        troughVal = troughVal * (1 - BASS_TROUGH_COEFF_DEC) + smoothVal * BASS_TROUGH_COEFF_DEC;
+    }
+    else {
+        troughVal = troughVal * (1 - BASS_TROUGH_COEFF_INC) + smoothVal * BASS_TROUGH_COEFF_INC;
+    }
+
+    // scale the output to a single byte without overshoot without taking trough value into consideration
+    float scaledOutput = (MIN(smoothVal, peakVal) / peakVal) * 255.0;  
+    // with trough value
+    float scaledOutputForBeatDetection = (MIN(smoothVal - troughVal, peakVal - troughVal) / (peakVal - troughVal)) * 255.0;
 
     setAnimationIntensity(scaledOutput);
 
@@ -81,6 +91,7 @@ void adcTransferComplete() {
     // HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)&midpointAvg, 4, 1);
     HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)&absVal, 4, 1);
     HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)&peakVal, 4, 1);
+    HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)&troughVal, 4, 1);
     HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)&smoothVal, 4, 1);
     HAL_UART_Transmit(&DEBUG_UART, (uint8_t *)&scaledOutput, 4, 1);
 
--- ledCollar_Firmware/Src/sound.h
+++ ledCollar_Firmware/Src/sound.h
@@ -5,15 +5,17 @@
 #define SAMPLING_DIV    4
 #define SAMPLING_RATE   (35750 / SAMPLING_DIV)
 #define TARGET_FREQ     75 // 45
-#define BLOCK_SIZE      20 // block size
+#define BLOCK_SIZE      32 // block size
 // #define ADC_CENTER      1430
 
-#define MIDPOINT_AVG_COEFF  0.002
-#define BASS_PEAK_COEFF_INC 0.2
-#define BASS_PEAK_COEFF_DEC (BLOCK_SIZE * 0.000001) // 0.0001 worked good at 50 binSize
-#define BASS_PEAK_MIN_VAL   (BLOCK_SIZE * 100) // 5000 worked good at 50 binSize
-#define BASS_PEAK_HOLD_TIME 600 // ms
-#define BASS_SMOOTH_COUNT   12
+#define MIDPOINT_AVG_COEFF      0.002
+#define BASS_PEAK_COEFF_INC     0.2
+#define BASS_PEAK_COEFF_DEC     (BLOCK_SIZE * 0.000001) // 0.0001 worked good at 50 binSize
+#define BASS_PEAK_MIN_VAL       (BLOCK_SIZE * 200) // 10000 worked good at 50 binSize
+#define BASS_PEAK_HOLD_TIME     600 // ms
+#define BASS_TROUGH_COEFF_DEC   0.2
+#define BASS_TROUGH_COEFF_INC   (BLOCK_SIZE * 0.00005)
+#define BASS_SMOOTH_COUNT       12
 
 #define NUM_MIC_SAMPLES (SAMPLING_DIV * BLOCK_SIZE)
 extern uint16_t micSamples[NUM_MIC_SAMPLES];
