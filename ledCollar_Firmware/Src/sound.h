da9c675bd02c8ace82305224f06757fdd18d8e48
LeoDJ <leandro.spaeth@gmail.com>
2020-10-31 17:34:07 +0100

    tweak sound detection

--- ledCollar_Firmware/Src/sound.h
+++ ledCollar_Firmware/Src/sound.h
@@ -5,14 +5,14 @@
 #define ADC_SAMPLING_CYCLES  240
 #define SAMPLING_DIV    4
 #define SAMPLING_RATE   (ADC_CLOCK / ADC_SAMPLING_CYCLES / SAMPLING_DIV)
-#define TARGET_FREQ     75 // 45
+#define TARGET_FREQ     45 // 45
 #define BLOCK_SIZE      32 // block size
 // #define ADC_CENTER      1430
 
 #define MIDPOINT_AVG_COEFF      0.002
 #define BASS_PEAK_COEFF_INC     0.2
-#define BASS_PEAK_COEFF_DEC     (BLOCK_SIZE * 0.000001) // 0.0001 worked good at 50 binSize
-#define BASS_PEAK_MIN_VAL       (BLOCK_SIZE * 200) // 10000 worked good at 50 binSize
+#define BASS_PEAK_COEFF_DEC     (BLOCK_SIZE * 0.0000015) // 0.0001 worked good at 50 binSize
+#define BASS_PEAK_MIN_VAL       (BLOCK_SIZE * 25) // 10000 worked good at 50 binSize
 #define BASS_PEAK_HOLD_TIME     600 // ms
 #define BASS_TROUGH_COEFF_DEC   0.2
 #define BASS_TROUGH_COEFF_INC   (BLOCK_SIZE * 0.00005)
